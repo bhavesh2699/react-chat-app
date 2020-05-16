054f59f14f5126531b12cb586648450ec14606e0
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-05-16 22:15:44 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const router = require('./router');
+const {addUser,removeUser,getUser,getUserInRoom} =  require('./users.js');
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app); 
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    socket.on('join',({name, room},callback)=>{
+
+        const {error, user } = addUser({id:socket.id, name, room});
+
+        if(error) return callback(error);
+
+        socket.emit('message', {user: 'admin', text:`${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', {user:'admin', text:`${user.name}, has joined`});
+
+        socket.join(user.room);
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) =>{
+
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', {user:user.name, text:message});
+        callback();
+
+
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user has left');
+    })
+
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server runing on ${PORT}`));
